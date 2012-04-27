242ed954f86223b20901cf266f3241c165ce1718
Mickaël Menu <mickael.menu@gmail.com>
2012-04-27 21:42:07 +0200

    Correction implém Position

--- src/pcps/components/Position.java
+++ src/pcps/components/Position.java
@@ -1,119 +1,49 @@
 package pcps.components;
 
-import pcps.services.BlocService;
 import pcps.services.Direction;
 import pcps.services.PositionService;
 import pcps.services.TerrainService;
-import pcps.services.TypeBloc;
 
-public class Position implements PositionService, TerrainService {
+public class Position implements
+	/* provides */
+	PositionService {
 
-	@Override
-	public PositionService getPosSortie() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	protected int largeur;
+	protected int hauteur;
+	protected int x;
+	protected int y;
 
-	@Override
-	public PositionService getPosHero() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public Position() {}
 
 	@Override
-	public BlocService getBlocHero() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public BlocService getBloc(PositionService pos) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public BlocService getBlocVersDirection(BlocService bloc, Direction dir) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isHeroVivant() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isDiamantsRestants() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isDeplacementBlocPossible(BlocService bloc, Direction dir) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void init(int l, int h) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void setBloc(TypeBloc type, int x, int y) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deplacerBlocVersDirection(BlocService bloc, Direction dir) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void fairePasDeMiseAJour() {
-		// TODO Auto-generated method stub
-
+	public void init(TerrainService terrain, int x, int y) {
+		largeur = terrain.getLargeur();
+		hauteur = terrain.getHauteur();
+		this.x = x % largeur;
+		this.y = y % hauteur;
 	}
 
 	@Override
 	public int getLargeur() {
-		// TODO Auto-generated method stub
-		return 0;
+		return largeur;
 	}
 
 	@Override
 	public int getHauteur() {
-		// TODO Auto-generated method stub
-		return 0;
+		return hauteur;
 	}
 
 	@Override
 	public int getX() {
-		// TODO Auto-generated method stub
-		return 0;
+		return x;
 	}
 
 	@Override
 	public int getY() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void init(TerrainService t, int x, int y) {
-		// TODO Auto-generated method stub
-
+		return y;
 	}
 
 	@Override
 	public void deplacerVersDirection(Direction dir) {
-		// TODO Auto-generated method stub
-
 	}
-
 }
